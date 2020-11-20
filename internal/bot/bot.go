ff88a2e409ddfd3e9a2a5d9abdffb631e4392b79
Dmitriy Efimov <EfimovDI@sibedge.com>
2020-11-20 16:31:32 +0300

    bot acts

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
-	"strings"
 	"tbot/config"
 	wiki2 "tbot/internal/wiki"
 )
@@ -49,21 +48,29 @@ func (b *Bot) Start() error {
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 		//msg.ReplyToMessageID = update.Message.MessageID
 
-		if strings.HasPrefix(msg.Text, "w ") {
-			query := msg.Text[2:]
-			title, err := b.wiki.Query(query)
-			if err != nil {
-				//log.Printf("error: %v", err)
-				msg.Text = fmt.Sprintf("error: %v", err)
-				b.bot.Send(msg)
-				continue
-			}
-			msg.Text = title
-			b.bot.Send(msg)
-			continue
-		}
+		msg.Text = b.Act(msg.Text)
 
-		b.bot.Send(msg)
+		_, err = b.bot.Send(msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+		}
 	}
 	return nil
+}
+
+func (b *Bot) Act(msgText string) string {
+	if len(msgText) < 3 {
+		return msgText
+	}
+	query := msgText[2:]
+	actName := msgText[:2]
+	switch actName {
+	case "w ":
+		title, err := b.wiki.Query(query)
+		if err != nil {
+			return fmt.Sprintf("error: %v", err)
+		}
+		return title
+	}
+	return msgText
 }
