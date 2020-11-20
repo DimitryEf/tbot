8484d781cbaa7bd906801b0bdc154028059c3a0b
Dmitriy Efimov <EfimovDI@sibedge.com>
2020-11-20 13:33:55 +0300

    bot struct

--- cmd/tbot/main.go
+++ cmd/tbot/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"log"
 	"tbot/config"
+	bot2 "tbot/internal/bot"
 	"tbot/internal/errors"
 )
 
@@ -11,28 +10,9 @@ func main() {
 	cfg, err := config.NewConfig()
 	errors.PanicIfErr(err)
 
-	bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	bot, err := bot2.NewBot(cfg)
 	errors.PanicIfErr(err)
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		//msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
-	}
+	err = bot.Start()
+	errors.PanicIfErr(err)
 }
---
+++ internal/bot/bot.go
@@ -0,0 +1,46 @@
+package bot
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"tbot/config"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(cfg *config.Config) (*Bot, error) {
+	bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	if err != nil {
+		return nil, err
+	}
+	bot.Debug = true
+	return &Bot{bot: bot}, nil
+}
+
+func (b *Bot) Start() error {
+	log.Printf("Authorized on account %s", b.bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := b.bot.GetUpdatesChan(u)
+	if err != nil {
+		return err
+	}
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		//msg.ReplyToMessageID = update.Message.MessageID
+
+		b.bot.Send(msg)
+	}
+	return nil
+}
