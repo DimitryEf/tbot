c36381faadc41d9cd4d9f3799f811ebbcad819be
DimitryEf <efimov-d-i@yandex.ru>
2020-12-10 09:01:48 +0300

    fix topicTags in answer

--- services/golang/golang.go
+++ services/golang/golang.go
@@ -5,7 +5,6 @@ import (
 	"gorm.io/gorm"
 	"strings"
 	"tbot/config"
-	"tbot/internal/errors"
 )
 
 type Golang struct {
@@ -15,12 +14,14 @@ type Golang struct {
 }
 
 func NewGolang(golangStg *config.GolangSettings, db *gorm.DB) *Golang {
-	err := db.AutoMigrate(&Topic{}, &Tag{})
-	errors.PanicIfErr(err)
-	db.Create(ConvertQueryToTopic("Get executable dir\n(tags: get executable dir)\n---\n\nex, err := os.Executable()\ndir := filepath.Dir(ex)\nfmt.Println(\"dir:\", dir)\n"))
-	db.Create(ConvertQueryToTopic("Extract beginning of string (prefix)\n(tags: extract beginning string prefix)\n---\n\nt := string([]rune(s)[:5])"))
-	db.Create(ConvertQueryToTopic("Extract string suffix\n(tags: extract string suffix)\n---\n\nt := string([]rune(s)[len([]rune(s))-5:])"))
-	db.Create(ConvertQueryToTopic("Exec other program\n(tags: exec program)\n---\n\nerr := exec.Command(\"program\", \"arg1\", \"arg2\").Run()"))
+	//err := db.AutoMigrate(&Topic{}, &Tag{})
+	//errors.PanicIfErr(err)
+	//db.Create(ConvertQueryToTopic("Get executable dir\n(tags: get executable dir)\n---\n\nex, err := os.Executable()\ndir := filepath.Dir(ex)\nfmt.Println(\"dir:\", dir)\n"))
+	//db.Create(ConvertQueryToTopic("Extract beginning of string (prefix)\n(tags: extract beginning string prefix)\n---\n\nt := string([]rune(s)[:5])"))
+	//db.Create(ConvertQueryToTopic("Extract string suffix\n(tags: extract string suffix)\n---\n\nt := string([]rune(s)[len([]rune(s))-5:])"))
+	//db.Create(ConvertQueryToTopic("Exec other program\n(tags: exec program)\n---\n\nerr := exec.Command(\"program\", \"arg1\", \"arg2\").Run()"))
+
+	//db.Create(ConvertQueryToTopic("Exec other program\n(tags: exec program)\n---\n\nerr := exec.Command(\"program\", \"arg1\", \"arg2\").Run()"))
 
 	//TODO do not do []Topic and then Create(&topics) the result in topic_tags will be bad
 
@@ -68,7 +69,12 @@ func (n *Golang) Query(query string) (string, error) {
 		set[topic.Title] = topic
 	}
 	for _, topic := range set {
-		res += "\n\n***\n" + fmt.Sprintf("%s\n(tags:%v)\n---\n%s", topic.Title, "tagsStr", topic.Code)
+		n.db.Model(&topic).Association("Tags").Find(&topic.Tags)
+		topicTags := ""
+		for _, tag := range topic.Tags {
+			topicTags += " " + tag.Name
+		}
+		res += "\n\n***\n" + fmt.Sprintf("%s\n(tags:%v)\n---\n%s", topic.Title, topicTags, topic.Code)
 	}
 	return res, nil
 }
