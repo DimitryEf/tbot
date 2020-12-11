91ef7bbf345ce1ee90e5afff4f57d6ac175b6259
DimitryEf <efimov-d-i@yandex.ru>
2020-12-11 19:37:20 +0300

    for heroku2

--- cmd/tbot/main.go
+++ cmd/tbot/main.go
@@ -35,7 +35,7 @@ func main() {
 	errors.PanicIfErr(err)
 
 	// load db
-	db, err := gorm.Open(sqlite.Open("/home/dim/projects/tbot/databases/golang.db"), &gorm.Config{
+	db, err := gorm.Open(sqlite.Open("databases/golang.db"), &gorm.Config{
 		Logger: logger.New(
 			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 			logger.Config{
