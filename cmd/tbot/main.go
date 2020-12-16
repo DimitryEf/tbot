fbce331018958ee923caa0733c71a80086f8e3c3
DimitryEf <efimov-d-i@yandex.ru>
2020-12-16 17:27:45 +0300

    env port

--- cmd/tbot/main.go
+++ cmd/tbot/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"html"
 	"log"
+	"net/http"
 	"os"
 	"tbot/config"
 	bot2 "tbot/internal/bot"
@@ -60,14 +63,25 @@ func main() {
 	bot, err := bot2.NewBot(cfg, serviceManager)
 	errors.PanicIfErr(err)
 
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	//})
-	//go func() {
-	//	log.Fatal(http.ListenAndServe(":80", nil))
-	//}()
+	// heroku
+	addr, err := determineListenAddress()
+	errors.PanicIfErr(err)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	go func() {
+		log.Fatal(http.ListenAndServe(addr, nil))
+	}()
 
 	// start bot to listen chat messages
 	err = bot.Start()
 	errors.PanicIfErr(err)
+}
+
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
